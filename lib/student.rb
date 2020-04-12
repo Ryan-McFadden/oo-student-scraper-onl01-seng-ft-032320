144cd8d67f17820b1bdca9726ff8bf48a2d88df5
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-12 09:04:07 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,8 @@ class Student
   def initialize(student_hash)
     student_hash.each do |key, value|
       self.send("#{key}=, value")
+    end
+    @@all << self
   end
 
   def self.create_from_collection(students_array)
